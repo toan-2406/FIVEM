81c7155cf2ccd9a21f3dc749d7f3fd2e50004f5c
toan-2406 <ntoan2884@gmail.com>
2022-06-04 23:10:15 +0700

    update

--- cart-shop/app.js
+++ cart-shop/app.js
@@ -1,35 +1,60 @@
 
 const Exitbtn = document.getElementById("close");
 const WrapperElm = document.getElementById("wrapper");
-const ImgElmItem = document.getElementById("wrapper-item");
-const ImgElm = document.getElementById("imgElm");
-const ModalElm = document.getElementById("modal");
-console.log(ImgElmItem);
-console.log(ModalElm);
-console.log(Exitbtn);
-const onMouseMove = (e) =>{
-    ModalElm.style.left = e.offsetX + 'px';
-    ModalElm.style.top = e.offsetY + 'px';
-    console.log(e.offsetX);
-    console.log(e.offsetY);
-  }
-ImgElmItem.addEventListener('mouseover', (e) => {
-    let target = e.target;
-    console.log("mouseover");
-    this.addEventListener('mousemove', onMouseMove);
-   if(target === ImgElm){
-    ModalElm.style.display = 'block';
-    ModalElm.style.zIndex = '1000';
+
+var ItemElms = document.querySelectorAll(".img");
+var ModalElm = document.querySelectorAll(".cart-wapper__infos");
+
+var TabElms = document.querySelectorAll(".category__item");
+var TabContentElms = document.querySelectorAll(".tab-content");
+
+var BuyNowElms = document.querySelectorAll(".btn-buy");
+var ModalBuyNowElms = document.querySelector(".modal-wrapper");
+console.log(TabElms)
+//Show modal when click on button buy now
+BuyNowElms.forEach(function(elm, index) {
+    elm.addEventListener("click", function() {
+        ModalBuyNowElms.classList.add("active");
+    });
+});
+ModalBuyNowElms.addEventListener("click", function(e) {
+    if (e.target.classList.contains("modal-wrapper")) {
+        ModalBuyNowElms.classList.remove("active");
     }
 });
-ImgElmItem.addEventListener('mouseout', (e) => {
-    let target = e.target;
-    console.log("mouseout");
-    this.removeEventListener('mousemove', onMouseMove);
-    if(target === ImgElm){
-    ModalElm.style.display = 'none';
-    }
+//Tab change category
+TabElms.forEach(function(elm, index) {
+    elm.addEventListener("click", function() {
+        TabElms.forEach(function(elm) {
+            elm.classList.remove("active");
+        });
+        this.classList.add("active");
+        TabContentElms.forEach(function(elm) {
+            elm.classList.remove("active");
+        });
+        TabContentElms[index].classList.add("active");
+    });
 });
+
+
+//active item when hover
+ItemElms.forEach((item,index) => {
+    item.addEventListener("mouseover", (e) => {   
+        item.addEventListener("mousemove", (e) => {
+            const offsetX = e.offsetX + 15;
+            const offsetY = e.offsetY + 20;
+            ModalElm[index].classList.add('active')
+            ModalElm[index].style.top = offsetY  + "px";
+            ModalElm[index].style.left = offsetX + "px";
+        });
+    });
+    item.addEventListener("mouseout", (e) => {
+        ModalElm[index].classList.remove('active')
+    });
+})
+
+
+//exit btn
 Exitbtn.addEventListener("click", function () {
     WrapperElm.classList.add("close");
 })
